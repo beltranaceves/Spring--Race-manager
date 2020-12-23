b10a7b2540290e768c4f97773e6e9a144e9aee5b
Melisa Barro Hermida <melisa.barro@udc.es>
2020-12-24 00:40:49 +0100

    [Func-4,5] Cambiado ClientMaxParticipantsException e implementado JsonToClientExceptionConversor para funcionalidades 4 y 5

--- ws-app-client/src/main/java/es/udc/ws/races/client/service/exceptions/ClientMaxParticipantsException.java
+++ ws-app-client/src/main/java/es/udc/ws/races/client/service/exceptions/ClientMaxParticipantsException.java
@@ -3,19 +3,25 @@ package es.udc.ws.races.client.service.exceptions;
 public class ClientMaxParticipantsException extends Exception {
 
     private Long raceId;
+    private int maxParticipants;
 
-    public ClientMaxParticipantsException(Long raceId) {
+    public ClientMaxParticipantsException(Long raceId, int maxParticipants) {
         super("Cannot inscribe to race with id = " + raceId +
                 " because all inscriptions are full.");
         this.raceId = raceId;
+        this.maxParticipants = maxParticipants;
     }
 
     public Long getRaceId() {
         return raceId;
     }
 
+    public int getMaxParticipants() { return maxParticipants; }
+
     public void setRaceId(Long raceId) {
         this.raceId = raceId;
     }
+
+    public void setMaxParticipants(int maxParticipants) { this.maxParticipants = maxParticipants; }
 
 }

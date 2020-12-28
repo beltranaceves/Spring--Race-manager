7c8029b893df727d54c0fda9af345657c6a27908
Melisa Barro Hermida <melisa.barro@udc.es>
2020-12-28 18:29:57 +0100

    [Func-5] Añadidos métodos toArrayNode en JsonToRestInscriptionDtoConversor y toRestInscriptionDtos en InscriptionToRestInscriptionDtoConversor, necesarios para la implementación de la funcionalidad 5

--- ws-app-service/src/main/java/es/udc/ws/app/restservice/dto/InscriptionToRestInscriptionDtoConversor.java
+++ ws-app-service/src/main/java/es/udc/ws/app/restservice/dto/InscriptionToRestInscriptionDtoConversor.java
@@ -2,10 +2,22 @@ package es.udc.ws.app.restservice.dto;
 
 import es.udc.ws.app.model.inscription.Inscription;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InscriptionToRestInscriptionDtoConversor {
 
     public static RestInscriptionDto toRestInscriptionDto(Inscription inscription) {
         return new RestInscriptionDto(inscription.getInscriptionId(), inscription.getRaceId(),
                 inscription.getDorsalNumber(), inscription.getCollected());
+    }
+
+    public static List<RestInscriptionDto> toRestInscriptionDtos(List<Inscription> inscriptions) {
+        List<RestInscriptionDto> inscriptionDtos = new ArrayList<>(inscriptions.size());
+        for (int i = 0; i < inscriptions.size(); i++) {
+            Inscription inscription = inscriptions.get(i);
+            inscriptionDtos.add(toRestInscriptionDto(inscription));
+        }
+        return inscriptionDtos;
     }
 }
--- ws-app-service/src/main/java/es/udc/ws/app/restservice/json/JsonToRestInscriptionDtoConversor.java
+++ ws-app-service/src/main/java/es/udc/ws/app/restservice/json/JsonToRestInscriptionDtoConversor.java
@@ -1,8 +1,11 @@
 package es.udc.ws.app.restservice.json;
 
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import es.udc.ws.app.restservice.dto.RestInscriptionDto;
+
+import java.util.List;
 
 public class JsonToRestInscriptionDtoConversor {
 
@@ -18,6 +21,18 @@ public class JsonToRestInscriptionDtoConversor {
                 put("collected", inscription.getCollected());
         
         return inscriptionNode;
+    }
+
+    public static ArrayNode toArrayNode(List<RestInscriptionDto> inscriptions) {
+
+        ArrayNode inscriptionsNode = JsonNodeFactory.instance.arrayNode();
+        for (int i = 0; i < inscriptions.size(); i++) {
+            RestInscriptionDto inscriptionDto = inscriptions.get(i);
+            ObjectNode inscriptionObject = toObjectNode(inscriptionDto);
+            inscriptionsNode.add(inscriptionObject);
+        }
+
+        return inscriptionsNode;
     }
 
 }

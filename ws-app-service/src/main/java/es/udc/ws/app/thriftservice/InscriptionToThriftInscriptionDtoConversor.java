0261f57048ee351533960a4eb9b7b5dc0a26e707
Melisa Barro Hermida <melisa.barro@udc.es>
2021-01-01 19:42:40 +0100

    [Func-5] Añadido método toThriftInscriptionDtos en el conversor

--- ws-app-service/src/main/java/es/udc/ws/app/thriftservice/InscriptionToThriftInscriptionDtoConversor.java
+++ ws-app-service/src/main/java/es/udc/ws/app/thriftservice/InscriptionToThriftInscriptionDtoConversor.java
@@ -3,6 +3,9 @@ package es.udc.ws.app.thriftservice;
 import es.udc.ws.app.model.inscription.Inscription;
 import es.udc.ws.app.thrift.ThriftInscriptionDto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InscriptionToThriftInscriptionDtoConversor {
 
     public static ThriftInscriptionDto toThriftInscriptionDto(Inscription inscription) {
@@ -11,4 +14,16 @@ public class InscriptionToThriftInscriptionDtoConversor {
                 inscription.getDorsalNumber(), inscription.getCollected());
 
     }
+
+    public static List<ThriftInscriptionDto> toThriftInscriptionDtos(List<Inscription> inscriptions) {
+
+        List<ThriftInscriptionDto> dtos = new ArrayList<>(inscriptions.size());
+
+        for (Inscription inscription : inscriptions) {
+            dtos.add(toThriftInscriptionDto(inscription));
+        }
+        return dtos;
+
+    }
+
 }

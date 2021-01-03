aa41c4c9e0da1a4322b787e30cfe3b9b28edb9c6
Dimitry Demyanov Prishtchepa <d.demyanov@udc.es>
2021-01-03 20:31:09 +0100

    [Común] Añadido otro constructor de ClientInscriptionDto

--- ws-app-client/src/main/java/es/udc/ws/app/client/service/dto/ClientInscriptionDto.java
+++ ws-app-client/src/main/java/es/udc/ws/app/client/service/dto/ClientInscriptionDto.java
@@ -2,81 +2,85 @@ package es.udc.ws.app.client.service.dto;
 
 public class ClientInscriptionDto {
 
-        private Long inscriptionId;
-        private Long raceId;
-        private int dorsalNumber;
-        private Boolean collected;
+    private Long inscriptionId;
+    private Long raceId;
+    private int dorsalNumber;
+    private Boolean collected;
+    private String creditCardNumber;
 
-        public ClientInscriptionDto(Long inscriptionId, Long raceId, int dorsalNumber, Boolean collected) {
-            this.inscriptionId = inscriptionId;
-            this.raceId = raceId;
-            this.dorsalNumber = dorsalNumber;
-            this.collected = collected;
-        }
+    public ClientInscriptionDto(Long inscriptionId, Long raceId, int dorsalNumber, Boolean collected) {
+        this.inscriptionId = inscriptionId;
+        this.raceId = raceId;
+        this.dorsalNumber = dorsalNumber;
+        this.collected = collected;
+    }
 
-        public Long getInscriptionId() {
-            return inscriptionId;
-        }
-        public void setInscriptionId(Long inscriptionId) {
-            this.inscriptionId = inscriptionId;
-        }
+    public ClientInscriptionDto(Long inscriptionId, String creditCardNumber) {
+        this.inscriptionId = inscriptionId;
+        this.creditCardNumber = creditCardNumber;
+    }
 
-        public Long getRaceId() {
-            return raceId;
-        }
-        public void setRaceId(Long raceId) {
-            this.raceId = raceId;
-        }
+    public Long getInscriptionId() { return inscriptionId; }
+    public void setInscriptionId(Long inscriptionId) {
+        this.inscriptionId = inscriptionId;
+    }
 
-        public int getDorsalNumber() { return dorsalNumber; }
-        public void setDorsalNumber(int dorsalNumber) {
-            this.dorsalNumber = dorsalNumber;
-        }
+    public Long getRaceId() {
+        return raceId;
+    }
+    public void setRaceId(Long raceId) {
+        this.raceId = raceId;
+    }
 
-        public Boolean getCollected() {
-            return collected;
-        }
-        public void setCollected(Boolean collected) {
-            this.collected = collected;
-        }
+    public int getDorsalNumber() { return dorsalNumber; }
+    public void setDorsalNumber(int dorsalNumber) {
+        this.dorsalNumber = dorsalNumber;
+    }
 
-        @Override
-        public int hashCode() {
-            final int prime = 31;
-            int result = 1;
-            result = prime * result + ((collected == null) ? 0 : collected.hashCode());
-            result = prime * result + dorsalNumber;
-            result = prime * result + ((inscriptionId == null) ? 0 : inscriptionId.hashCode());
-            result = prime * result + ((raceId == null) ? 0 : raceId.hashCode());
-            return result;
-        }
+    public Boolean getCollected() {
+        return collected;
+    }
+    public void setCollected(Boolean collected) {
+        this.collected = collected;
+    }
 
-        @Override
-        public boolean equals(Object obj) {
-            if (this == obj)
-                return true;
-            if (obj == null)
-                return false;
-            if (getClass() != obj.getClass())
-                return false;
-            ClientInscriptionDto other = (ClientInscriptionDto) obj;
-            if (collected == null) {
-                if (other.collected != null)
-                    return false;
-            } else if (!collected.equals(other.collected))
-                return false;
-            if (dorsalNumber != other.dorsalNumber)
-                return false;
-            if (inscriptionId == null) {
-                if (other.inscriptionId != null)
-                    return false;
-            } else if (!inscriptionId.equals(other.inscriptionId))
-                return false;
-            if (raceId == null) {
-                if (other.raceId != null)
-                    return false;
-            } else if (!raceId.equals(other.raceId))
-                return false;
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((collected == null) ? 0 : collected.hashCode());
+        result = prime * result + dorsalNumber;
+        result = prime * result + ((inscriptionId == null) ? 0 : inscriptionId.hashCode());
+        result = prime * result + ((raceId == null) ? 0 : raceId.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
             return true;
-        }
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ClientInscriptionDto other = (ClientInscriptionDto) obj;
+        if (collected == null) {
+            if (other.collected != null)
+                return false;
+        } else if (!collected.equals(other.collected))
+            return false;
+        if (dorsalNumber != other.dorsalNumber)
+            return false;
+        if (inscriptionId == null) {
+            if (other.inscriptionId != null)
+                return false;
+        } else if (!inscriptionId.equals(other.inscriptionId))
+            return false;
+        if (raceId == null) {
+            if (other.raceId != null)
+                return false;
+        } else if (!raceId.equals(other.raceId))
+            return false;
+        return true;
+    }
 }
